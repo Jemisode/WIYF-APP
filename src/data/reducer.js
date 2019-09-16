fa42da997090e880874901c3125396b44d273a3c
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-16 16:50:29 +0100

    setup file structure, created basic data files

---
+++ src/data/reducer.js
@@ -0,0 +1,9 @@
+import initial from './initial';
+
+const reducer = (state, action) => {
+    switch (action.type) { 
+        
+    }; 
+};
+
+export default reducer;
---
+++ src/data/store.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from "redux";
+
+// needed to implement redux-localstorage
+// import persistState from 'redux-localstorage';
+
+// needed to implement thunk
+// import thunk from "redux-thunk";
+
+import initial from './initial';
+import reducer from './reducer';
+
+// needed to implement redux-localstorage && thunk with redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer, 
+    initial, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // needed to implement redux-localstorage
+    // composeEnhancers(persistState()),
+    // needed to implement thunk
+    // composeEnhancers(applyMiddleware(thunk)),
+);
+
+export default store;
