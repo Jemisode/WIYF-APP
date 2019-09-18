7a6c2549512f58eabb7f734753aa55b52d9513e0
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-18 22:25:46 +0100

    changed title text colour

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-    <h1><Link style={{ textDecoration: "none", color: "black" }} to="/" >What's In Your Fridge</Link></h1>
+    <h1><Link style={{ textDecoration: "none", color: "#169DB2" }} to="/" >What's In Your Fridge</Link></h1>
 );
 export default Header;
