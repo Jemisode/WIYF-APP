b84487e496414592c5acafe222c4339751303226
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-18 22:21:11 +0100

    created api method for Search component + adjusted styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from '../../axios';
 
 class Search extends Component {
     constructor(props) {
@@ -6,7 +7,9 @@ class Search extends Component {
 
         this.state = {
             ingredient: "",
-            error: false
+            error: false,
+            ingredients: [],
+            loaded: false
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -22,8 +25,10 @@ class Search extends Component {
     } 
 
     handleClick() {
+        let { chosenIngredients, handleIngredient } = this.props;
+        let { ingredients } = this.state;
+
         let ingredient = this.state.ingredient;
-        let { ingredients, chosenIngredients, handleIngredient } = this.props;
 
         if (chosenIngredients.length <= 2 && ingredients.includes(ingredient) && !chosenIngredients.includes(ingredient)) {
             handleIngredient(ingredient);
@@ -38,50 +43,68 @@ class Search extends Component {
         this.props.handleRemoveIngredient(ingredient);
     }
 
+    componentDidMount() {
+        axios.get("/ingredients").then(({ data }) => {
+            let ingredients = [];
+            data.map(ingredient => {
+                // response includes ingredient objects, with id and name keys
+                ingredients = [...ingredients, ingredient.name]
+            });
+            this.setState({
+                loaded: true,
+                // sorting ingredients into alphabetical order
+                ingredients: ingredients.sort()
+            });
+        });
+    }
+
     render() {
-        let { ingredient, error } = this.state;
-        let { ingredients, chosenIngredients } = this.props;
+        let { ingredient, error, ingredients, loaded } = this.state;
+        let { chosenIngredients } = this.props;
 
-        return (
-            <div className="card bg-info text-white d-flex align-items-center" >
-                <form className="">
-                    <div className="form-group">
-                        <label htmlFor="ingredient-choice" >Pick Your Ingredients</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            onChange={ (e) => this.handleChange(e) }
-                            value={ ingredient }
-                            list="ingredients"
-                            id="ingredient-choice"
-                        />
+        return !loaded ? <p>Loading...</p> : (
+            <div className="d-flex flex-column justify-content-center text-center" style={{ height: "70vh" }}>
+                <div className="card bg-info text-white d-flex align-items-center">
+                    <form className="">
+                        <div className="form-group" style={{ maxWidth: 200 }}>
+                            <label htmlFor="ingredient-choice">Pick Your Ingredients</label>
 
-                        <datalist id="ingredients">
-                            { ingredients.map((ingredient, index) => (
-                                <option key={ index } value={ ingredient } />
-                            )) }
-                        </datalist>
+                            <input
+                                type="text"
+                                className="form-control"
+                                onChange={ (e) => this.handleChange(e) }
+                                value={ ingredient }
+                                list="ingredients"
+                                id="ingredient-choice"
+                            />
 
-                        <button type="button" className="btn btn-primary btn-block" onClick={ this.handleClick }>Add</button>
-                        { error ? <p className="text-danger">Please pick up to 3 different ingredients, from our list</p> : null }
-                    </div>
+                            <datalist id="ingredients">
+                                { ingredients.map((ingredient, index) => (
+                                    <option key={ index } value={ ingredient } />
+                                )) }
+                            </datalist>
 
-                    <div className="form-group">
-                        <h3>Ingredients</h3>
-                        <ul className="col">
-                            { chosenIngredients.map((ingredient, index) => (
-                                <div key={ index } className="row">
-                                    <li><h5>{ ingredient }</h5></li>
-                                    <button onClick={ (e) => this.handleDelete(e, ingredient) } className="btn btn-danger btn-sm">X</button>
-                                </div>
-                        )) }
-                    </ul>
-        
-                    <button type="submit" className="btn btn-primary btn-block" >Find Recipes</button>
+                            <button type="button" className="btn btn-primary btn-block" onClick={ this.handleClick }>Add</button>
+
+                            { error ? <p className="text-warning">Please pick up to 3 different ingredients, from our list</p> : null }
+                        </div>
+
+                        <div className="form-group">
+                            <h3>Ingredients</h3>
+                            <ul className="col">
+                                { chosenIngredients.map((ingredient, index) => (
+                                    <div key={ index } className="row">
+                                        <li><h5>{ ingredient }</h5></li>
+                                        <button onClick={ (e) => this.handleDelete(e, ingredient) } className="btn btn-danger btn-sm">X</button>
+                                    </div>
+                                )) }
+                            </ul>
+                
+                            <button type="submit" className="btn btn-primary btn-block" >Find Recipes</button>
+                        </div> 
+                    </form>
                 </div>
-                    
-            </form>
-        </div>
+            </div>
         );
     };
 }
