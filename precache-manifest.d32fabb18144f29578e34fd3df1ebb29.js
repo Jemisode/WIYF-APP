2f8e36dcc87c75df0141acef9a0f1e5d624c3291
Jess <jessmatthews@live.co.uk>
2019-09-20 09:48:24 +0100

    Updates

---
+++ precache-manifest.d32fabb18144f29578e34fd3df1ebb29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1374c88ed82cb8d5cb1c34a0b9cdef09",
+    "url": "/WIYF-APP/index.html"
+  },
+  {
+    "revision": "a069cef5b6e822d7ed41",
+    "url": "/WIYF-APP/static/css/main.9d682920.chunk.css"
+  },
+  {
+    "revision": "37076732ee9f66165eed",
+    "url": "/WIYF-APP/static/js/2.9c1d64ea.chunk.js"
+  },
+  {
+    "revision": "a069cef5b6e822d7ed41",
+    "url": "/WIYF-APP/static/js/main.20fed7c9.chunk.js"
+  },
+  {
+    "revision": "7aa7a07d074e17c5541a",
+    "url": "/WIYF-APP/static/js/runtime~main.e1691395.js"
+  },
+  {
+    "revision": "b2fa037f555fc137b308c1ffa4ec741f",
+    "url": "/WIYF-APP/static/media/fridge.b2fa037f.png"
+  },
+  {
+    "revision": "f63d4fd4a6d9539ad58dc549c463e3a2",
+    "url": "/WIYF-APP/static/media/tomato.f63d4fd4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e243bc9c2109bdc042d7b219055bdb0.js"
+  "/WIYF-APP/precache-manifest.d32fabb18144f29578e34fd3df1ebb29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WIYF-APP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
